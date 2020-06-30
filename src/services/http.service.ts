3eed7d24a4c121a312b70255153e6bba8fb2787d
Szymon <saicxs123@gmail.com>
2020-06-30 14:42:36 +0200

    Update http.service.ts

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -11,61 +11,65 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
   getSongs(): Observable<Music[]> {
-    return this.http.get<Music[]>('http://localhost:8080/songs');
+    return this.http.get<Music[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/songs');
   }
 
   searchSongs(text: string): Observable<Music[]> {
-    return this.http.get<Music[]>('http://localhost:8080/songs/' + text);
+    return this.http.get<Music[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/songs/' + text);
   }
 
   addSong(music: Music, playlistname: string) {
-    this.http.post<any>('http://localhost:8080/addYTVideoToPlaylist', { song: music, playlist: playlistname })
+    this.http.post<any>(
+      'https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/addYTVideoToPlaylist',
+      { song: music, playlist: playlistname })
     .subscribe();
   }
 
   getPlaylists(videoId: string = null){
     if (videoId) {
-      return this.http.get<any[]>('http://localhost:8080/songPlaylists/' + videoId);
+      return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/songPlaylists/' + videoId);
     }
     else {
-      return this.http.get<any[]>('http://localhost:8080/playlists');
+      return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/playlists');
     }
   }
 
   getArtists(){
-    return this.http.get<any[]>('http://localhost:8080/artists');
+    return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/artists');
   }
 
   getArtist(artist: string){
-    return this.http.get<any[]>('http://localhost:8080/artist/' + artist);
+    return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/artist/' + artist);
   }
 
   getPlaylist(playlist: string){
-    return this.http.get<any[]>('http://localhost:8080/playlist/' + playlist);
+    return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/playlist/' + playlist);
   }
 
   setSong(song: Music){
-    this.http.post<any>('http://localhost:8080/setSong', { song })
+    this.http.post<any>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/setSong', { song })
     .subscribe();
   }
 
   deleteSong(music: Music, playlistname: string){
-    this.http.post<any>('http://localhost:8080/deleteYTVideoFromPlaylist', { song: music, playlist: playlistname })
+    this.http.post<any>(
+      'https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/deleteYTVideoFromPlaylist',
+      { song: music, playlist: playlistname })
     .subscribe();
   }
 
   changeArtist(music: Music) {
-    this.http.post<any>('http://localhost:8080/changeArtist', { music })
+    this.http.post<any>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/changeArtist', { music })
     .subscribe();
   }
 
   getHistory(){
-    return this.http.get<any>('http://localhost:8080/getHistory');
+    return this.http.get<any>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/getHistory');
   }
 
   setHistory(historyArray: Music[]){
     const history = { history: JSON.stringify(historyArray) };
-    this.http.post<any>('http://localhost:8080/setHistory', { history })
+    this.http.post<any>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/setHistory', { history })
     .subscribe();
   }
 }
