a6ef7f8ccdb84a99c9ecccef9c105fb378f6e450
Szymon <saicxs123@gmail.com>
2020-06-30 15:01:56 +0200

    Update http.service.ts

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -11,65 +11,62 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
   getSongs(): Observable<Music[]> {
-    return this.http.get<Music[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/songs');
+    return this.http.get<Music[]>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/songs`);
   }
 
   searchSongs(text: string): Observable<Music[]> {
-    return this.http.get<Music[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/songs/' + text);
+    return this.http.get<Music[]>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/songs/` + text);
   }
 
   addSong(music: Music, playlistname: string) {
-    this.http.post<any>(
-      'https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/addYTVideoToPlaylist',
-      { song: music, playlist: playlistname })
+    this.http.post<any>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/addYTVideoToPlaylist`,
+    { song: music, playlist: playlistname })
     .subscribe();
   }
 
   getPlaylists(videoId: string = null){
     if (videoId) {
-      return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/songPlaylists/' + videoId);
+      return this.http.get<any[]>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/songPlaylists/` + videoId);
     }
     else {
-      return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/playlists');
+      return this.http.get<any[]>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/playlists`);
     }
   }
 
   getArtists(){
-    return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/artists');
+    return this.http.get<any[]>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/artists`);
   }
 
   getArtist(artist: string){
-    return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/artist/' + artist);
+    return this.http.get<any[]>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/artist/` + artist);
   }
 
   getPlaylist(playlist: string){
-    return this.http.get<any[]>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/playlist/' + playlist);
+    return this.http.get<any[]>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/playlist/` + playlist);
   }
 
   setSong(song: Music){
-    this.http.post<any>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/setSong', { song })
+    this.http.post<any>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/setSong`, { song })
     .subscribe();
   }
 
   deleteSong(music: Music, playlistname: string){
-    this.http.post<any>(
-      'https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/deleteYTVideoFromPlaylist',
-      { song: music, playlist: playlistname })
+    this.http.post<any>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/deleteYTVideoFromPlaylist`, { song: music, playlist: playlistname })
     .subscribe();
   }
 
   changeArtist(music: Music) {
-    this.http.post<any>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/changeArtist', { music })
+    this.http.post<any>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/changeArtist`, { music })
     .subscribe();
   }
 
   getHistory(){
-    return this.http.get<any>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/getHistory');
+    return this.http.get<any>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/getHistory`);
   }
 
   setHistory(historyArray: Music[]){
     const history = { history: JSON.stringify(historyArray) };
-    this.http.post<any>('https://hobby-fgmnaacjcnamgbkepkeibfel.dbs.graphenedb.com:24780/db/data/setHistory', { history })
+    this.http.post<any>(`https://myspotify-app.herokuapp.com:${process.env.PORT}/setHistory`, { history })
     .subscribe();
   }
 }
